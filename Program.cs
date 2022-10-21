865e1d597b38c3a287409decc69b5d0529326de4
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-21 21:20:07 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day15BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("welcome to Binary Search Tree");
+        }
+    }
+}
